6e6de5372081feb7b4933ec49e516ae371a6d912
JessyHiller <821214930@qq.com>
2020-10-23 16:51:39 +0800

    update .gitignore

---
+++ code/SkipGram.py
@@ -0,0 +1,4 @@
+# pre-train word embeddings on each DataSet with SkipGram
+# Mikolov, T.; Sutskever, I.; Chen, K.; Corrado, G. S.; and Dean, J. 2013.
+# Distributed representations of words and phrases and their compositionality. In NIPS, 3111–3119.
+
