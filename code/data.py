ac8774a05f0d7c4b93e35b3e3b2243f85df653b7
JessyHiller <821214930@qq.com>
2020-10-25 23:01:20 +0800

    1025

---
+++ code/data.py
@@ -0,0 +1,78 @@
+import numpy as np
+
+
+def load_word_embedding(path, corpus):
+    word_set = set()
+    for docs in corpus:
+        words = docs.strip().split()
+        for w in words:
+            word_set.add(w)
+
+    word_embeddings = []
+    word_dict = dict()
+    word_dict['$EOF$'] = 0  # add EOF
+    word_embeddings.append(np.zeros(200))
+    index = 1
+    with open(path, 'r', encoding='utf8') as f:
+        for line in f.readlines():
+            line = line.strip().split(' ')
+            if len(line) == 2:
+                continue
+            w = line[0]
+            if w not in word_set:
+                continue
+            word_dict[w] = index
+            vector = [float(i) for i in line[-200:]]
+            word_embeddings.append(vector)
+            index += 1
+    return np.array(word_embeddings), word_dict
+
+
+def lookup(word_dict, reviews, max_sen_len, max_doc_len):
+    X = []
+    for doc in reviews:
+        i = 0
+        x = np.zeros((max_doc_len, max_sen_len), type=int)
+        for sen in doc.split('<sssss>'):
+            if i == max_doc_len:
+                break
+            j = 0
+            for w in sen.strip().split(' '):
+                if j == max_sen_len:
+                    break
+                x[i][j] = word_dict[w]
+                j += 1
+        i += 1
+        X.append(x)
+    return X
+
+
+class DataSet(object):
+    def __init__(self, data_path):
+        self.t_usr = []
+        self.t_prd = []
+        self.t_label = []
+        self.t_docs = []
+        with open(data_path, 'r', encoding='utf8') as f:
+            for line in f.readlines():
+                terms = line.strip().split('\t\t')
+                self.t_usr.append(terms[0])
+                self.t_prd.append(terms[1])
+                self.t_label.append(int(terms[2])-1)
+                self.t_docs.append(terms[3].lower())
+        self.data_size = len(self.t_docs)
+
+    def usr_prd_dict(self):
+        usr_dict = dict()
+        prd_dict = dict()
+        for u in self.t_usr:
+            i = 0
+            if usr_dict.get(u) is None:
+                usr_dict[u] = i
+                i += 1
+        for p in self.t_prd:
+            i = 0
+            if prd_dict.get(p) is None:
+                prd_dict[p] = i
+                i += 1
+        return usr_dict, prd_dict
--- code/model.py
+++ code/model.py
@@ -1,1 +1,43 @@
+import torch
+
+
+class HUAPA(object):
+    def __init__(self, embedding_file, hidden_size, max_doc_len, max_sen_len, lr, batch_size, u_num, p_num):
+        self.embedding_file = embedding_file
+        self.embedding_dim = embedding_file.shape()[-1]
+        self.hidden_size = hidden_size
+        self.max_doc_len = max_doc_len
+        self.max_sen_len = max_sen_len
+        self.lr = lr
+        self.no_review = batch_size
+        self.optimizer = torch.optim.Adam(lr=lr)
+        self.embedding = torch.nn.Embedding.from_pretrained(torch.tensor(embedding_file))
+        self.usr_embedding = torch.nn.Embedding(u_num, 2*self.hidden_size)
+        self.prd_embedding = torch.nn.Embedding(p_num, 2*self.hidden_size)
+        self.w2s_LSTM = torch.nn.LSTM(input_size=self.embedding_dim, hidden_size=hidden_size, bidirectional=True)
+        self.s2d_LSTM = torch.nn.LSTM(input_size=self.hidden_size*2, hidden_size=hidden_size, bidirectional=True)
+
+    def look_up(self, X, uid, pid):
+        """
+        :param X: (no_review, max_doc_len, max_sen_len)
+        :param uid: int
+        :param pid: int
+        :return: (no_review, max_doc_len, max_sen_len, embedding_dim)
+        """
+        return self.embedding(X), self.usr_embedding(uid), self.prd_embedding(pid)
+
+    def w2s(self, X):
+        """
+        :param: X (no_review, max_doc_len, max_sen_len, embedding_dim)
+        biLSTM input of shape(seq_len, batch, input_size)
+        output, (h_n, c_n)
+                 output of shape(seq_len, batch, num_directions*hidden_size)
+        :return (batch, seq_len, num_directions*hidden_size)
+        """
+        input = X.view((self.no_review*self.max_doc_len, self.max_sen_len, self.embedding_dim))
+        input.permute_(0, 1)
+        output, (_, _) = self.w2s_LSTM(input)
+        output.permute_(0, 1)
+        return output
+
 
